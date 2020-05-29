426cc52ed354e73c1464eb7eb3f6d6388cfd3840
Anton Stephanyak <anton.stephanyak@gmail.com>
2020-05-29 19:16:57 +0500

    env test

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 require_once "./api/config/database.php";
 
-echo getenv('JAWSDB_MARIA_URL');
+
+
+$isHeroku = getenv('JAWSDB_MARIA_URL');
+if($isHeroku) {
+    echo "Ok! We got a Heroku env var! :)";
+}else{
+    echo "Local development";
+}
